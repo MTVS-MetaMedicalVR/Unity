a15c9f417e39f051d9f26524e2939df3c00d3985
DONGSULEEE <dslee313@gmail.com>
2024-10-24 17:56:26 +0900

    스냅 수정중

--- VR/Assets/04. Scripts/UnitChairAnimator.cs
+++ VR/Assets/04. Scripts/UnitChairAnimator.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class UnitChairAnimator : MonoBehaviour
 {
     public Animator animator;
+    public bool isBase = false;
+    public bool isBack = false;
+    public bool isLight = false;
+    public bool isTable = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,41 +25,49 @@ public class UnitChairAnimator : MonoBehaviour
     public void TableOn()
     {
         animator.SetTrigger("TableOn");
+        isTable = true;
     }
 
     public void TableOff()
     {
         animator.SetTrigger("TableOff");
+        isTable = false;
     }
 
     public void BackOn()
     {
         animator.SetTrigger("BackOn");
+        isBack = true;
     }
 
     public void BackOff()
     {
         animator.SetTrigger("BackOff");
+        isBack = false;
     }
 
     public void BaseOn()
     {
         animator.SetTrigger("BaseOn");
+        isBase = true;
     }
 
     public void BaseOff()
     {
         animator.SetTrigger("BaseOff");
+        isBase = false;
     }
 
     public void LightOn()
     {
         animator.SetTrigger("LightOn");
+        isLight = true;
     }
 
     public void LightOff()
     {
         animator.SetTrigger("LightOff");
+        isLight = false;
     }
 
 }
