b99ba941d599d3412969c140864c6289bed608b3
DONGSULEEE <149301885+DONGSULEEE@users.noreply.github.com>
2024-11-04 18:44:23 +0900

    머리 돌리는거

--- VR/Assets/04. Scripts/JawFollow.cs
+++ VR/Assets/04. Scripts/JawFollow.cs
@@ -34,9 +34,11 @@ public class JawFollow : MonoBehaviour
 
         nowRotation = Mathf.Clamp(nowRotation, -30f, 0f);
 
-        Vector3 openDir = Quaternion.AngleAxis(nowRotation, jawRoot.parent.forward) * jawRoot.parent.right;
+        Vector3 openDir = (Quaternion.AngleAxis(nowRotation, jawRoot.parent.forward) * jawRoot.parent.right);
 
-        transform.rotation = Quaternion.LookRotation(jawRoot.parent.forward, openDir);
+
+
+        transform.rotation = Quaternion.LookRotation(-Vector3.right, openDir);
 
         //transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,transform.localEulerAngles.y, nowRotation);
     }
