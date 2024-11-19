f726f6170e775e35e6e596b5a518b52143b67a45
jaehyeond <pjhyun9711@gmail.com>
2024-11-19 18:11:32 +0900

    입까지 잘 다뭄

--- VR/Assets/06. Jaehyeon/Scripts/STT(CLOVA)/STTClient.cs
+++ VR/Assets/06. Jaehyeon/Scripts/STT(CLOVA)/STTClient.cs
@@ -7,8 +7,7 @@ using System.Text;
 public class STTClient : MonoBehaviour
 {
     private string apiUrl = "http://192.168.204.27:8000/hoonjang_stt"; // Python 서버 URL
-    public List<JawFollow> jawControllers = new List<JawFollow>(); // JawFollow 스크립트 리스트
-    public List<HeadFollow> headControllers = new List<HeadFollow>(); // HeadFollow 스크립트 리스트
+    public static event System.Action<string> OnSTTResponseReceived; // STT 응답 이벤트
 
     public void SendAudioData(byte[] wavData)
     {
@@ -41,65 +40,8 @@ public class STTClient : MonoBehaviour
             else
             {
                 Debug.Log("서버 응답: " + request.downloadHandler.text);
-                HandleSTTResponse(request.downloadHandler.text);
-            }
-        }
-    }
-
-    private void HandleSTTResponse(string response)
-    {
-        // 특정 명령어에 따라 Jaw와 Head 동작 제어
-        foreach (var jawController in jawControllers)
-        {
-            if (jawController != null)
-            {
-                if (response.Contains("아 해보세요"))
-                {
-                    jawController.isOpen = true; // 입 벌리기
-                    Debug.Log("환자 입 벌리기 동작 실행");
-                }
-                else if (response.Contains("입을 닫아보세요"))
-                {
-                    jawController.isOpen = false; // 입 닫기
-                    Debug.Log("환자 입 닫기 동작 실행");
-                }
-                else
-                {
-                    Debug.Log("인식된 텍스트 (Jaw 관련): " + response + " (해당하는 동작 없음)");
-                }
-            }
-            else
-            {
-                Debug.LogWarning("JawFollow 컨트롤러가 설정되지 않았습니다.");
-            }
-        }
-
-        foreach (var headController in headControllers)
-        {
-            if (headController != null)
-            {
-                if (response.Contains("왼쪽으로 고개를 돌려보세요"))
-                {
-                    headController.isLeft = true;
-                    headController.isRight = false;
-                    Debug.Log("환자 왼쪽으로 고개 돌리기 동작 실행");
-                }
-                else if (response.Contains("오른쪽으로 고개를 돌려보세요"))
-                {
-                    headController.isLeft = false;
-                    headController.isRight = true;
-                    Debug.Log("환자 오른쪽으로 고개 돌리기 동작 실행");
-                }
-                else
-                {
-                    headController.isLeft = false;
-                    headController.isRight = false; // 기본 상태로 복원
-                    Debug.Log("인식된 텍스트 (Head 관련): " + response + " (해당하는 동작 없음)");
-                }
-            }
-            else
-            {
-                Debug.LogWarning("HeadFollow 컨트롤러가 설정되지 않았습니다.");
+                // 이벤트를 통해 응답 전달
+                OnSTTResponseReceived?.Invoke(request.downloadHandler.text);
             }
         }
     }
---
+++ VR/Assets/06. Jaehyeon/Scripts/STT(CLOVA)/Test(STT)/ActionHandler.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionHandler : MonoBehaviour
+{
+    //public List<JawFollow> jawControllers = new List<JawFollow>();
+    //public List<HeadFollow> headControllers = new List<HeadFollow>();
+    public JawFollow jawController;  // 단일 JawFollow
+    public HeadFollow headController;  // 단일 HeadFollow
+
+    private void OnEnable()
+    {
+        // STTClient의 이벤트 구독
+        STTClient.OnSTTResponseReceived += HandleSTTResponse;
+    }
+
+    private void OnDisable()
+    {
+        // STTClient의 이벤트 구독 해제
+        STTClient.OnSTTResponseReceived -= HandleSTTResponse;
+    }
+
+    private void HandleSTTResponse(string response)
+    {
+        if (response.Contains("아 해보세요"))
+        {
+            HandleAction("OpenMouth");
+        }
+        else if (response.Contains("다 물어보세요"))
+        {
+            HandleAction("CloseMouth");
+        }
+        else if (response.Contains("좌측으로 돌려보세요"))
+        {
+            HandleAction("TurnLeftHead");
+        }
+        else if (response.Contains("우측으로 돌려보세요"))
+        {
+            HandleAction("TurnRightHead");
+        }
+        else
+        {
+            Debug.Log("인식된 텍스트: " + response + " (해당하는 동작 없음)");
+        }
+    }
+
+    public void HandleAction(string action)
+    {
+        switch (action)
+        {
+            case "OpenMouth":
+                if (jawController != null)
+                {
+                    jawController.isOpen = true;
+                    Debug.Log("환자 입 벌리기 동작 실행");
+                }
+                else
+                {
+                    Debug.LogWarning("JawFollow 컨트롤러가 설정되지 않았습니다.");
+                }
+                break;
+
+            case "CloseMouth":
+                if (jawController != null)
+                {
+                    jawController.isOpen = false;
+                    Debug.Log("환자 입 닫기 동작 실행");
+                }
+                else
+                {
+                    Debug.LogWarning("JawFollow 컨트롤러가 설정되지 않았습니다.");
+                }
+                break;
+
+            case "TurnLeftHead":
+                if (headController != null)
+                {
+                    headController.isLeft = true;
+                    Debug.Log("환자 왼쪽으로 고개 돌리기 동작 실행");
+                }
+                else
+                {
+                    Debug.LogWarning("HeadFollow 컨트롤러가 설정되지 않았습니다.");
+                }
+                break;
+
+            case "TurnRightHead":
+                if (headController != null)
+                {
+                    headController.isRight = true;
+                    Debug.Log("환자 오른쪽으로 고개 돌리기 동작 실행");
+                }
+                else
+                {
+                    Debug.LogWarning("HeadFollow 컨트롤러가 설정되지 않았습니다.");
+                }
+                break;
+
+            default:
+                Debug.Log("알 수 없는 동작: " + action);
+                break;
+        }
+    }
+
+}
